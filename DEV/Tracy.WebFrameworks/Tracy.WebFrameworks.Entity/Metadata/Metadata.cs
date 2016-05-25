2bb5ea99beef8bc21e15fade9dcb2635aaa87eae
mcgrady_525 <mcgrady_525@163.com>
2016-05-25 15:23:43 +0800

    删除所有T4模板，需要重建。

--- DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.Entity/Metadata/Metadata.cs
+++ DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.Entity/Metadata/Metadata.cs
@@ -6,68 +6,68 @@ using System.Text;
 
 namespace Tracy.WebFrameworks.Entity.Metadata
 {
-    /// <summary>
-    /// Corporation
-    /// </summary>
-    [DataContract]
-    public class CorporationMetadata: CorporationMetadataBase
-    {
+    ///// <summary>
+    ///// Corporation
+    ///// </summary>
+    //[DataContract]
+    //public class CorporationMetadata: CorporationMetadataBase
+    //{
 
-    }
+    //}
 
-    /// <summary>
-    /// Department
-    /// </summary>
-    [DataContract]
-    public class DepartmentMetadata : DepartmentMetadataBase
-    {
+    ///// <summary>
+    ///// Department
+    ///// </summary>
+    //[DataContract]
+    //public class DepartmentMetadata : DepartmentMetadataBase
+    //{
 
-    }
+    //}
 
-    /// <summary>
-    /// Employee
-    /// </summary>
-    [DataContract]
-    public class EmployeeMetadata : EmployeeMetadataBase
-    {
+    ///// <summary>
+    ///// Employee
+    ///// </summary>
+    //[DataContract]
+    //public class EmployeeMetadata : EmployeeMetadataBase
+    //{
 
-    }
+    //}
 
-    /// <summary>
-    /// Role
-    /// </summary>
-    [DataContract]
-    public class RoleMetadata : RoleMetadataBase
-    {
+    ///// <summary>
+    ///// Role
+    ///// </summary>
+    //[DataContract]
+    //public class RoleMetadata : RoleMetadataBase
+    //{
 
-    }
+    //}
 
-    /// <summary>
-    /// Menu
-    /// </summary>
-    [DataContract]
-    public class MenuMetadata : MenuMetadataBase
-    {
+    ///// <summary>
+    ///// Menu
+    ///// </summary>
+    //[DataContract]
+    //public class MenuMetadata : MenuMetadataBase
+    //{
 
-    }
+    //}
 
-    /// <summary>
-    /// Function
-    /// </summary>
-    [DataContract]
-    public class FunctionMetadata : FunctionMetadataBase
-    {
+    ///// <summary>
+    ///// Function
+    ///// </summary>
+    //[DataContract]
+    //public class FunctionMetadata : FunctionMetadataBase
+    //{
 
-    }
+    //}
 
-    /// <summary>
-    /// RoleFunction
-    /// </summary>
-    [DataContract]
-    public class RoleFunctionMetadata : RoleFunctionMetadataBase
-    {
+    ///// <summary>
+    ///// RoleFunction
+    ///// </summary>
+    //[DataContract]
+    //public class RoleFunctionMetadata : RoleFunctionMetadataBase
+    //{
 
-    }
+    //}
 
 
 }
