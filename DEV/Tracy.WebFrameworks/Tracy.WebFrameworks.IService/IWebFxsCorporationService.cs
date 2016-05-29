0f9161678711c87b07dba175fa37fc957f0f8df5
mcgrady_525 <mcgrady_525@163.com>
2016-05-29 18:24:26 +0800

    ...

--- DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.IService/IWebFxsCorporationService.cs
+++ DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.IService/IWebFxsCorporationService.cs
@@ -11,6 +11,45 @@ namespace Tracy.WebFrameworks.IService
     [ServiceContract(ConfigurationName = "WebFxsCorporationService.IWebFxsCorporationService")]
     public interface IWebFxsCorporationService
     {
+        #region IRepository
+        /// <summary>
+        /// 查询所有
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract]
+        WebFxsResult<IEnumerable<Corporation>> GetAll();
 
+        /// <summary>
+        /// 依据id查询
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [OperationContract]
+        WebFxsResult<Corporation> GetById(int id);
+
+        /// <summary>
+        /// 插入
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [OperationContract]
+        WebFxsResult<Corporation> Insert(Corporation item);
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [OperationContract]
+        WebFxsResult<bool> Update(Corporation item);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [OperationContract]
+        WebFxsResult<bool> Delete(int id); 
+        #endregion
     }
 }
--- DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.Service/WebFxsCorporationService.svc.cs
+++ DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.Service/WebFxsCorporationService.svc.cs
@@ -8,38 +8,154 @@ using Tracy.WebFrameworks.Entity;
 using Tracy.WebFrameworks.Entity.CommonBO;
 using Tracy.WebFrameworks.IService;
 using Tracy.WebFrameworks.Data;
+using Tracy.WebFrameworks.Common.Helper;
 
 namespace Tracy.WebFrameworks.Service
 {
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall, ConcurrencyMode = ConcurrencyMode.Multiple)]
     //[WcfServiceCounter(SystemCode = "WebFrameworks", Source = "Offline.Service")]
-    public class WebFxsCorporationService: IWebFxsCorporationService
+    public class WebFxsCorporationService : IWebFxsCorporationService
     {
+        #region IRepository
         /// <summary>
-        /// 批量插入公司信息
+        /// 查询所有
         /// </summary>
-        /// <param name="list"></param>
         /// <returns></returns>
-        public WebFxsResult<bool> BatchInsert(List<Corporation> list)
+        public WebFxsResult<IEnumerable<Corporation>> GetAll()
         {
-            var result = new WebFxsResult<bool> 
+            var result = new WebFxsResult<IEnumerable<Corporation>>()
             {
-                ReturnCode= ReturnCodeType.Error
+                ReturnCode = ReturnCodeType.Error,
+                Content = new List<Corporation>()
             };
+            DBHelper.NoLockInvokeDB(() =>
+            {
+                using (var db = new WebFrameworksDB())
+                {
+                    result.ReturnCode = ReturnCodeType.Success;
+                    result.Content = db.Corporation;
+                }
+            });
+            return result;
+        }
 
+        /// <summary>
+        /// 依据id查询
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public WebFxsResult<Corporation> GetById(int id)
+        {
+            var result = new WebFxsResult<Corporation>()
+            {
+                ReturnCode = ReturnCodeType.Error,
+                Content = new Corporation()
+            };
+            DBHelper.NoLockInvokeDB(() =>
+            {
+                using (var db = new WebFrameworksDB())
+                {
+                    result.ReturnCode = ReturnCodeType.Success;
+                    result.Content = db.Corporation.FirstOrDefault(p => p.CorporationID == id);
+                }
+            });
+            return result;
+        }
+
+        /// <summary>
+        /// 插入
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public WebFxsResult<Corporation> Insert(Corporation item)
+        {
+            var result = new WebFxsResult<Corporation>()
+            {
+                ReturnCode = ReturnCodeType.Error,
+                Content = new Corporation()
+            };
+            //CRUD Operation in Connected mode
             using (var db = new WebFrameworksDB())
             {
-                db.Corporation.AddRange(list);
-                if (db.SaveChanges()>0)
+                result.Content = db.Corporation.Add(item);
+                if (db.SaveChanges() > 0)
                 {
-                    result.Content = true;
                     result.ReturnCode = ReturnCodeType.Success;
                 }
             }
 
             return result;
         }
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public WebFxsResult<bool> Update(Corporation item)
+        {
+            var result = new WebFxsResult<bool>()
+            {
+                ReturnCode = ReturnCodeType.Error,
+                Content = false
+            };
+            //CRUD Operation in Connected mode
+            using (var db = new WebFrameworksDB())
+            {
+                var corporation = db.Corporation.FirstOrDefault(p => p.CorporationID == item.CorporationID);
+                if (corporation != null)
+                {
+                    corporation.ParentCorpID = item.ParentCorpID;
+                    corporation.CorporationCode = item.CorporationCode;
+                    corporation.CorporationName = item.CorporationName;
+                    corporation.Address = item.Address;
+                    corporation.CreatedBy = item.CreatedBy;
+                    corporation.CreatedTime = item.CreatedTime;
+                    corporation.LastUpdatedBy = item.LastUpdatedBy;
+                    corporation.LastUpdatedTime = item.LastUpdatedTime;
+                }
+                if (db.SaveChanges() > 0)
+                {
+                    result.ReturnCode = ReturnCodeType.Success;
+                    result.Content = true;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public WebFxsResult<bool> Delete(int id)
+        {
+            var result = new WebFxsResult<bool>()
+            {
+                ReturnCode = ReturnCodeType.Error,
+                Content = false
+            };
+            //CRUD Operation in Connected mode
+            using (var db = new WebFrameworksDB())
+            {
+                var corporation = db.Corporation.FirstOrDefault(p => p.CorporationID == id);
+                if (corporation != null)
+                {
+                    db.Corporation.Remove(corporation);
+                }
+
+                if (db.SaveChanges() > 0)
+                {
+                    result.ReturnCode = ReturnCodeType.Success;
+                    result.Content = true;
+                }
+            }
+
+            return result;
+        }
+        #endregion
 
     }
 }
