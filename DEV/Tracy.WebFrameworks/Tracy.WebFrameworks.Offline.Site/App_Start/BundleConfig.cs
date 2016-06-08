71ebc4be8e81c1dd5f2b527f0995f31c1266411e
mcgrady_525 <mcgrady_525@163.com>
2016-06-08 21:50:41 +0800

    ...

--- DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.Offline.Site/App_Start/BundleConfig.cs
+++ DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.Offline.Site/App_Start/BundleConfig.cs
@@ -12,24 +12,15 @@ namespace Tracy.WebFrameworks.Offline.Site
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jeasyui").Include(
+            bundles.Add(new ScriptBundle("~/bundles/easyui").Include(
                         "~/Scripts/easyui/jquery.easyui.min.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
-
             //css
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/easyui").Include(
                       "~/Content/easyui/default/easyui.css",
-                      "~/Content/easyui/icon.css",
-                      "~/Content/site.css"));
+                      "~/Content/easyui/icon.css"));
+            bundles.Add(new StyleBundle("~/Content/site").Include(
+                        "~/Content/Site.css"));
         }
     }
 }
