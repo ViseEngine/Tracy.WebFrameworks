d14b2ca6e11e46c48421646dc50b7e5398a2b2b6
mcgrady_525 <mcgrady_525@163.com>
2016-06-14 15:14:45 +0800

    add Tracy.WebFrameworks架构图.UML

---
+++ DEV/Tracy.WebFrameworks/Tracy.WebFrameworks.IService/IWebFxsEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracy.WebFrameworks.IService
+{
+    [ServiceContract(ConfigurationName = "WebFxsEmployeeService.IWebFxsEmployeeService")]
+    public interface IWebFxsEmployeeService
+    {
+
+    }
+}
